53133ce7ee749656b93fea90ff53da0228f96169
BrotanFire <brotishkafire@mail.ru>
2023-03-11 22:09:32 +0500

    Добавлена главная форма

---
+++ TestNetProg1/MainForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace TestNetProg1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Button_Reg_Runner = new System.Windows.Forms.Button();
+            this.Button_Donate_Runner = new System.Windows.Forms.Button();
+            this.Button_More_Info = new System.Windows.Forms.Button();
+            this.Button_Registration = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1904, 147);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(715, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(616, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MARATHON SKILLS 2016";
+            // 
+            // Button_Reg_Runner
+            // 
+            this.Button_Reg_Runner.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Button_Reg_Runner.AutoSize = true;
+            this.Button_Reg_Runner.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Button_Reg_Runner.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button_Reg_Runner.Location = new System.Drawing.Point(401, 165);
+            this.Button_Reg_Runner.Name = "Button_Reg_Runner";
+            this.Button_Reg_Runner.Size = new System.Drawing.Size(1079, 200);
+            this.Button_Reg_Runner.TabIndex = 1;
+            this.Button_Reg_Runner.Text = "Я хочу стать бегуном";
+            this.Button_Reg_Runner.UseVisualStyleBackColor = true;
+            // 
+            // Button_Donate_Runner
+            // 
+            this.Button_Donate_Runner.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Button_Donate_Runner.AutoSize = true;
+            this.Button_Donate_Runner.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button_Donate_Runner.Location = new System.Drawing.Point(401, 371);
+            this.Button_Donate_Runner.Name = "Button_Donate_Runner";
+            this.Button_Donate_Runner.Size = new System.Drawing.Size(1079, 200);
+            this.Button_Donate_Runner.TabIndex = 2;
+            this.Button_Donate_Runner.Text = "Я хочу стать спонсором бегуна";
+            this.Button_Donate_Runner.UseVisualStyleBackColor = true;
+            // 
+            // Button_More_Info
+            // 
+            this.Button_More_Info.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Button_More_Info.AutoSize = true;
+            this.Button_More_Info.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button_More_Info.Location = new System.Drawing.Point(401, 577);
+            this.Button_More_Info.Name = "Button_More_Info";
+            this.Button_More_Info.Size = new System.Drawing.Size(1079, 200);
+            this.Button_More_Info.TabIndex = 3;
+            this.Button_More_Info.Text = "Я хочу узнать больше о событии ";
+            this.Button_More_Info.UseVisualStyleBackColor = true;
+            // 
+            // Button_Registration
+            // 
+            this.Button_Registration.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Button_Registration.AutoSize = true;
+            this.Button_Registration.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button_Registration.Location = new System.Drawing.Point(1589, 841);
+            this.Button_Registration.Name = "Button_Registration";
+            this.Button_Registration.Size = new System.Drawing.Size(315, 63);
+            this.Button_Registration.TabIndex = 4;
+            this.Button_Registration.Text = "Login";
+            this.Button_Registration.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DimGray;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(841, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(345, 74);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "     Москва, Россия\r\nсреда 21 октября 2016";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.DimGray;
+            this.flowLayoutPanel1.Controls.Add(this.label3);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 910);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1904, 131);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.BackColor = System.Drawing.Color.DimGray;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1901, 131);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "None дней None часов и None минут до старта марафона\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.Button_Reg_Runner);
+            this.Controls.Add(this.Button_Registration);
+            this.Controls.Add(this.Button_More_Info);
+            this.Controls.Add(this.Button_Donate_Runner);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Button_Reg_Runner;
+        private System.Windows.Forms.Button Button_Donate_Runner;
+        private System.Windows.Forms.Button Button_More_Info;
+        private System.Windows.Forms.Button Button_Registration;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label3;
+    }
+}
--- TestNetProg1/MainForm.cs
+++ TestNetProg1/MainForm.cs
@@ -10,11 +10,18 @@ using System.Windows.Forms;
 
 namespace TestNetProg1
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
     }
+
 }
